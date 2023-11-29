02af3adc71922b29ebf386c75b97d3d87988176c
aknair95 <aakashunair01031995@gmail.com>
2023-11-29 22:59:31 +0530

    routes configured for components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import TaskList from "./components/taskList/taskList";
+import AddTask from "./components/addTask/addTask";
 
 function App() {
   return (
     <div>
-     
+     <BrowserRouter>
+          <div>
+            <Routes>
+              <Route path="/" element={<TaskList/>}/>
+              <Route path="/addTask" element={<AddTask/>}/>
+            </Routes>
+          </div>
+      </BrowserRouter>
     </div>
   );
 }
